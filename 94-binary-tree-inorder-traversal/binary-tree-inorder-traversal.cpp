1f15b4779aadefb81b0ae90f7b146f50e5f4193b
Abhinav Garg <83314128+ombhinav@users.noreply.github.com>
2024-06-28 17:09:20 +0530

    Time: 4 ms (31.90%) | Memory: 9.9 MB (38.95%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -11,14 +11,45 @@
  */
 class Solution {
 public:
-    vector<int> ans;
+
+    //recursion
+
+    // vector<int> ans;
+    // void inorder(TreeNode* root){
+    //     if(root == nullptr){
+    //         return;
+    //     }
+    //     inorder(root->left);
+    //     ans.push_back(root->val);
+    //     inorder(root->right);
+    // }
+
+
+    //morris traversal
+    vector<int>ans;
     void inorder(TreeNode* root){
-        if(root == nullptr){
-            return;
+        TreeNode* cur = root;
+        while(cur != NULL){
+            if(cur -> left == NULL){
+                ans.push_back(cur->val);
+                cur = cur -> right;
+            }
+            else{
+                TreeNode* prev = cur -> left;
+                while(prev -> right && prev -> right != cur){
+                    prev = prev -> right;
+                }
+                if(prev -> right == NULL){
+                    prev -> right = cur;
+                    cur = cur -> left;
+                }
+                else{
+                    prev -> right = NULL;
+                    ans.push_back(cur->val);
+                    cur = cur -> right;
+                }
+            }
         }
-        inorder(root->left);
-        ans.push_back(root->val);
-        inorder(root->right);
     }
     
     vector<int> inorderTraversal(TreeNode* root) {
