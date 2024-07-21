7a00e4bbcab1e9965c3c4cfe8167a340969e9b33
Abhinav Garg <83314128+ombhinav@users.noreply.github.com>
2024-07-22 02:30:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.cpp
+++ Difficulty: Medium/Topological sort/topological-sort.cpp
@@ -21,19 +21,56 @@ class Solution
 	//Function to return list containing vertices in Topological order. 
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
-	    int vis[V] = {0};
-	    stack<int> st;
-	    for(int i=0;i<V;i++){
-	        if(!vis[i]){
-	            dfs(i,vis,st,adj);
-	        }
-	    }
-	    vector<int> ans;
-	    while(!st.empty()){
-	        ans.push_back(st.top());
-	        st.pop();
-	    }
-	    return ans;
+	    //for dfs
+	    
+	   // int vis[V] = {0};
+	   // stack<int> st;
+	   // for(int i=0;i<V;i++){
+	   //     if(!vis[i]){
+	   //         dfs(i,vis,st,adj);
+	   //     }
+	   // }
+	   // vector<int> ans;
+	   // while(!st.empty()){
+	   //     ans.push_back(st.top());
+	   //     st.pop();
+	   // }
+	   // return ans;
+	   
+	   
+	   //for bfs
+	   int indegree[V] = {0};
+	   for(int i=0;i<V;i++){
+	       for(auto it: adj[i]){
+	           indegree[it]++;
+	       }
+	   }
+	   
+	   queue<int>q;
+	   for(int i=0;i<V;i++){
+	       if(indegree[i] == 0){
+	           q.push(i);
+	       }
+	   }
+	   
+	   vector<int> topo;
+	   while(!q.empty()){
+	       int node = q.front();
+	       q.pop();
+	       topo.push_back(node);
+	       
+	       for(auto it: adj[node]){
+	           indegree[it]--;
+	           if(indegree[it] == 0){
+	               q.push(it);
+	           }
+	       }
+	       
+	   }
+	   return topo;
+	   
+	   
+	   
 	}
 };
 
