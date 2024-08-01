bac58596bc8c9f9d03d5800d17ea8c0c68e8f200
Abhinav Garg <83314128+ombhinav@users.noreply.github.com>
2024-08-01 23:31:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
+++ Difficulty: Medium/Dijkstra Algorithm/dijkstra-algorithm.cpp
@@ -10,30 +10,30 @@ class Solution
     //from the source vertex S.
     vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
     {
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int, int>>> pq;
-        vector<int> dist(V);
-        for(int i=0;i<V;i++) dist[i] = 1e9;
         
+       
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
+        vector<int> dist(V+1,1e9);
         dist[S] = 0;
-        pq.push({0,S});
         
+        pq.push({0, S});
         while(!pq.empty()){
-            int dis  = pq.top().first;
-            int node = pq.top().second;
+            auto it = pq.top();
+            int node = it.second;
+            int dis = it.first;
             pq.pop();
             
-            for(auto it: adj[node]){
-                int edgeWeight = it[1];
+            for(auto& it: adj[node]){
                 int adjNode = it[0];
-                
+                int edgeWeight = it[1];
                 if(dis + edgeWeight < dist[adjNode]){
                     dist[adjNode] = dis + edgeWeight;
-                    pq.push({dist[adjNode], adjNode});
+                    pq.push({dist[adjNode],adjNode});
                 }
             }
         }
-        return dist;
         
+        return dist;
         
     }
 };
