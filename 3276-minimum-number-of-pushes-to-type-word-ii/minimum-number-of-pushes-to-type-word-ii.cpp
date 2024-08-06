c9fda4cf0c38b0c66141a4662f9dd9e5a17fb7d6
Abhinav Garg <83314128+ombhinav@users.noreply.github.com>
2024-08-06 23:11:24 +0530

    Time: 72 ms (62.04%) | Memory: 27.7 MB (14.43%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    //give one more try
+    int minimumPushes(string word) {
+        vector<int> letterFreq(26, 0);
+        for (char c : word) {
+            letterFreq[c - 'a']++;
+        }
+        
+        sort(letterFreq.rbegin(), letterFreq.rend());
+        
+        int totalPresses = 0;
+        for (int i = 0; i < 26; i++) {
+            if (letterFreq[i] == 0) break;
+            totalPresses += (i / 8 + 1) * letterFreq[i];
+        }
+        
+        return totalPresses;
+    }
+};
