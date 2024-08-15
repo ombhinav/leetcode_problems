e9b9fee10b824967c8add65a9571d9badfe73c68
Abhinav Garg <83314128+ombhinav@users.noreply.github.com>
2024-08-16 01:00:37 +0530

    Time: 78 ms (30.17%) | Memory: 86.1 MB (32.71%) - LeetSync

--- 890-lemonade-change/lemonade-change.cpp
+++ 890-lemonade-change/lemonade-change.cpp
@@ -1,84 +1,44 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        // int cnt5=0,cnt10=0,cnt20=0;
-        // for(int i=0;i<bills.size();i++){
-        //     if(bills[i] == 5){
-        //         cnt5++;
-        //     }
-        //     if(bills[i] == 10){
-        //         cnt10++;
-        //         if(cnt5 != 0){
-        //             cnt5--;
-        //         }
-        //         else{
-        //             return false;
-        //         }
-        //     }
-        //     if(bills[i] == 20){
-        //         cnt20++;
-        //         if(cnt5 != 0 && cnt10 != 0){
-        //             cnt10--;
-        //             cnt5--;
-        //         }
-        //         else if(cnt10 == 0 && cnt5 != 0){
-        //             if(cnt5 >= 3){
-        //                 cnt5--;
-        //                 cnt5--;
-        //                 cnt5--;
-        //                 if(cnt5 < 0){
-        //                     return false;
-        //                 }
-        //             }
-        //             else{
-        //                 return false;
-        //             }
-        //         }
-        //         else{
-        //             return false;
-        //         }
-        //     }
-        // }
-        // return true;
-        int n = bills.size();
-        int c1 = 0;
-        int c2 = 0;
-        for(int i=0;i<n;i++)
-        {
-            if(bills[i] == 5)
-            {
-                c1++;
+        int cnt5=0,cnt10=0,cnt20=0;
+        for(int i=0;i<bills.size();i++){
+            if(bills[i] == 5){
+                cnt5++;
             }
-            else if(bills[i] == 10)
-            {
-                if(c1>0)
-                {
-                    c1--;
-                    c2++;
+            if(bills[i] == 10){
+                cnt10++;
+                if(cnt5 != 0){
+                    cnt5--;
                 }
-                else
-                {
+                else{
                     return false;
                 }
             }
-            else if(bills[i] == 20)
-            {
-                if(c1>0 && c2 >0)
-                {
-                    c1--;
-                    c2--;
+            if(bills[i] == 20){
+                cnt20++;
+                if(cnt5 != 0 && cnt10 != 0){
+                    cnt10--;
+                    cnt5--;
                 }
-                else if(c1>=3)
-                {
-                    c1-=3;
+                else if(cnt10 == 0 && cnt5 != 0){
+                    if(cnt5 >= 3){
+                        cnt5--;
+                        cnt5--;
+                        cnt5--;
+                        if(cnt5 < 0){
+                            return false;
+                        }
+                    }
+                    else{
+                        return false;
+                    }
                 }
-                else
-                {
+                else{
                     return false;
                 }
             }
         }
         return true;
-    
     }
 };
