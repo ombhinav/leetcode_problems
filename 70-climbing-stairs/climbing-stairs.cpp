296067a7376d9a092eb9064fbc6cb5a7c1a4d2cf
Abhinav Garg <83314128+ombhinav@users.noreply.github.com>
2024-08-19 05:32:16 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (26.12%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -12,6 +12,15 @@ public:
     int climbStairs(int n) {
         //memoization
         vector<int> dp(n+1,-1);
-        return func(n,dp);
+        // return func(n,dp);
+
+        //tabulation
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+
     }
 };
