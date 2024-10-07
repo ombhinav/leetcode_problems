d5bf4e8ed6a446efd23cee527c943b3d93fab354
Abhinav Garg <83314128+ombhinav@users.noreply.github.com>
2024-10-07 18:22:27 +0530

    Time: 27 ms (5.05%) | Memory: 11.7 MB (31.24%) - LeetSync

---
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool stillGoing(string &s1, string &s2, string &s, bool &flag){
+        if(s.length()<2){
+            flag = false;
+            return false;
+        }
+        for(int i=0;i<=s.length()-2;i++){
+            string sub = s.substr(i,2);
+            if(sub == s1 || sub == s2){
+                s.erase(i,2);
+                flag = true;
+                return true;
+            }
+       }
+       flag = false;
+       return false;
+    }
+    int minLength(string s) {
+       if(s.length()<2){
+            return s.length();
+       } 
+       string s1 = "AB";
+       string s2 = "CD";
+       string ans = "";
+       bool flag = true;
+       while(flag){
+            stillGoing(s1,s2,s,flag);
+            cout<<s;
+            cout<<endl;
+       }
+       
+       return s.length();
+    }
+};
