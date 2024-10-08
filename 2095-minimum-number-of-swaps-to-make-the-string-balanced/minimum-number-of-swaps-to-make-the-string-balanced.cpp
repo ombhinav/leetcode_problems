c5324979ee50cf48de8f5f78da64ee161e05a497
Abhinav Garg <83314128+ombhinav@users.noreply.github.com>
2024-10-08 19:07:00 +0530

    Time: 98 ms (80.19%) | Memory: 32.6 MB (91.34%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,24 +1,40 @@
 class Solution {
 public:
     int minSwaps(string s) {
-        int n = s.size();
-        stack<int> st;
-        int unbalanced=0;
+        // int n = s.size();
+        // stack<int> st;
+        // int unbalanced=0;
+        // for(auto ch: s){
+        //     if(ch == '['){
+        //         st.push(char);
+        //     }
+        //     else{
+        //         if(!st.empty()){
+        //             st.pop();
+        //         }
+        //         else{
+        //             unbalanced++;
+        //         }
+        //     }
+        // }
+        // //returning half as unbalanced is counting the brackets
+        // //but one swap would fix 2 brackets at once, so halving 
+        // return (unbalanced+1)/2;
+
+
+        //without stack
+        int size = 0;
         for(auto ch: s){
             if(ch == '['){
-                st.push(ch);
+                size++;
             }
             else{
-                if(!st.empty()){
-                    st.pop();
-                }
-                else{
-                    unbalanced++;
+                if(size>0){
+                    size--;
                 }
             }
         }
-        //returning half as unbalanced is counting the brackets
-        //but one swap would fix 2 brackets at once, so halving 
-        return (unbalanced+1)/2;
+        return (size+1)/2;
+
     }
 };
