cebb509c28b0097e27bf89caf2d19cd0e086cb79
Abhinav Garg <83314128+ombhinav@users.noreply.github.com>
2024-10-09 07:01:35 +0530

    Time: 31 ms (16.37%) | Memory: 23 MB (75.23%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -44,23 +44,34 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals){
         vector<vector<int>> ans;
         sort(intervals.begin(),intervals.end());
+        // for(int i=0;i<intervals.size();i++){
+        //     int start = intervals[i][0];
+        //     int end = intervals[i][1];
+            
+        //     if(!ans.empty() && end <= ans.back()[1]){
+        //         continue;
+        //     }
+            
+        //     for(int j=i+1;j<intervals.size();j++){
+        //         if(intervals[j][0] <= end){
+        //             end = max(end, intervals[j][1]);
+        //         }
+        //         else{
+        //             break;
+        //         }
+        //     }
+        //     ans.push_back({start,end});
+        // }
+        // return ans;
+
+        //optimal
         for(int i=0;i<intervals.size();i++){
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-            
-            if(!ans.empty() && end <= ans.back()[1]){
-                continue;
+            if(ans.empty() || intervals[i][0] > ans.back()[1]){
+                ans.push_back(intervals[i]);
             }
-            
-            for(int j=i+1;j<intervals.size();j++){
-                if(intervals[j][0] <= end){
-                    end = max(end, intervals[j][1]);
-                }
-                else{
-                    break;
-                }
+            else{
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             }
-            ans.push_back({start,end});
         }
         return ans;
         
