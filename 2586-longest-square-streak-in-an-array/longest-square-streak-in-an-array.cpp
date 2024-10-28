ee895023b9b1c6a951106e6d4bd8b8fe701c547c
Abhinav Garg <83314128+ombhinav@users.noreply.github.com>
2024-10-29 01:01:25 +0530

    Time: 86 ms (66.67%) | Memory: 108.9 MB (75.24%) - LeetSync

--- 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
@@ -1,21 +1,45 @@
 class Solution {
 public:
     int longestSquareStreak(vector<int>& nums) {
-        unordered_map<int,int> mpp;
-        sort(nums.begin(),nums.end());
-        int maxStreak = 0;
-        for(auto num: nums){
-            //to check if the no. is a perfect square or not
-            int root = (int)sqrt(num);
-            if(root*root == num && mpp.find(root) != mpp.end()){
-                mpp[num] = mpp[root]+1;
+        // unordered_map<int,int> mpp;
+        // sort(nums.begin(),nums.end());
+        // int maxStreak = 0;
+        // for(auto num: nums){
+        //     //to check if the no. is a perfect square or not
+        //     int root = (int)sqrt(num);
+        //     if(root*root == num && mpp.find(root) != mpp.end()){
+        //         mpp[num] = mpp[root]+1;
 
+        //     }
+        //     else{
+        //         mpp[num] = 1;
+        //     }
+        //     maxStreak = max(maxStreak,mpp[num]);
+        // }
+        // return maxStreak < 2 ? -1 : maxStreak;
+
+
+    
+        unordered_set<int>st(nums.begin(),nums.end());
+        int maxStreak = 0;
+        // unordered_set<int>st;
+
+        // for(auto num: nums){
+        //     st.insert(num);
+        // }
+        for(int &num: nums){
+
+            int streak = 0;
+            long long cur = num;
+            while(st.find(cur) != st.end()){
+                streak++;
+                if(cur*cur > 1e5){
+                    break;
+                }
+                cur = cur*cur;
             }
-            else{
-                mpp[num] = 1;
-            }
-            maxStreak = max(maxStreak,mpp[num]);
+            maxStreak = max(maxStreak, streak);
         }
-        return maxStreak < 2 ? -1 : maxStreak;
+        return maxStreak < 2 ? -1 : maxStreak; 
     }
 };
