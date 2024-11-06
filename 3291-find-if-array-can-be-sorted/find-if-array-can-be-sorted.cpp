bff96f2b25f693f32264ce3b1d8d659da81db7ec
Abhinav Garg <83314128+ombhinav@users.noreply.github.com>
2024-11-07 00:52:59 +0530

    Time: 16 ms (11.51%) | Memory: 32.1 MB (25.80%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
@@ -6,7 +6,7 @@ public:
         sort(original.begin(),original.end());
 
         int n = __builtin_popcount(8);
-        for(int i=0;i<100;i++){
+        for(int i=0;i<nums.size();i++){
 
             for(int i=0;i<nums.size()-1;i++){
                 if(__builtin_popcount(nums[i]) == __builtin_popcount(nums[i+1])){
